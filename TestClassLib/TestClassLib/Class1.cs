eda9f9a8dca165845fc01a8b2e55178060552631
Abi Ali <abi@davyntt.com>
2014-05-28 11:36:38 -0400

    Project added

---
+++ TestClassLib/TestClassLib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLib
+{
+
+    // Hello1.cs
+    public class Hello1
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
